1115b3ab09a3959903a7b348cb818834b02ffb6b
Bruce Collie <brucecollie82@gmail.com>
2019-09-06 12:40:16 +0100

    Starting to work on stack machine

---
+++ rosette/app.rkt
@@ -0,0 +1,8 @@
+#lang rosette/safe
+
+(provide main)
+
+(require "stack.rkt")
+
+(define (main . args)
+  (void))
---
+++ rosette/stack.rkt
@@ -0,0 +1,37 @@
+#lang rosette/safe
+
+(require "interpreter.rkt")
+
+(require rosette/lib/match)
+
+; Implements a basic stack machine with a small instruction set with the aim of
+; trying out program synthesis using it.
+; 
+; Stacks are implemented as lists - the top of the stack is just the first
+; element of the list.
+
+(provide (all-defined-out))
+
+; Look at the top of the stack but don't remove it
+(define (peek stack)
+  (car stack))
+
+; Push a value onto the stack
+(struct push (val) #:transparent)
+
+; Pop the top value off the stack
+(struct pop () #:transparent)
+
+; Pop the two values from the top of the stack, then push their sum
+(struct add () #:transparent)
+
+; A small-step semantics function for stack programs. The state is a list (as
+; defined above to represent a stack).
+(define (step instruction state)
+  (match instruction
+    [(push val)  (cons val state)]
+    [(pop)       (cdr state)]
+    [(add)       (cons (+ (first state) (second state))
+                       (cddr state))]))
+
+(define interpreter (make-interpreter null step))
