a0bfe4bceabe604410a3392eb731909b760da56d
Bruce Collie <brucecollie82@gmail.com>
2019-10-15 15:10:52 +0100

    Allow simpl to be built

---
+++ get-simpl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ROOT=$(realpath $(dirname "$0"))
+
+INSTALL_DIR="$ROOT/install"
+SIMPL_DIR="$ROOT/simpl"
+
+if [ -d "$SIMPL_DIR" ]; then
+  echo "Removing existing simpl dir..."
+  rm -rf "$SIMPL_DIR"
+fi
+
+echo "Cloning simpl..."
+
+git clone git@github.com:kupl/SimplPublic.git "$SIMPL_DIR"
+cd "$SIMPL_DIR"
+
+echo "Building simpl..."
+
+chmod +x ./build
+./build
+
+echo "Installing simpl..."
+
+mkdir -p "$INSTALL_DIR/bin"
+cp ./main.native "$INSTALL_DIR/bin/simpl"
