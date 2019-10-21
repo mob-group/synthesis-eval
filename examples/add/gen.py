46d70b686b538ae00f8843c1d0a46dc43725d9fe
Jackson Woodruff <s1988171@monaco.inf.ed.ac.uk>
2019-10-21 15:40:53 +0100

    Update add and array_inc to match the C implemetations

--- examples/add/gen.py
+++ examples/add/gen.py
@@ -5,25 +5,24 @@ import random
 
 
 def generate_example():
-    array_len = random.randint(1, 3)
+    array_len = random.randint(1, 30)
 
     arr1 = []
-    arr2 = []
+    diff = random.randint(0, 100)
     res = []
 
     for i in range(array_len):
-        n1 = random.randint(0, 10)
-        n2 = random.randint(0, 10)
-        arr1.append(n1)
-        arr2.append(n2)
-        res.append(n1 + n2)
+        n = random.randint(-100, 100)
+        arr1.append(n)
+        res.append(n + diff)
 
-    return (arr1, arr2, res)
+    return (arr1, diff, res)
 
 
 # This tool is independent of the syntool name.
 def convert(example, example_class, syntool_name):
-    example_class.two_array_input(example[0], example[1])
+    example_class.add_array_input(example[0])
+    example_class.add_int_input(example[1])
     example_class.array_output(example[2])
 
     return example_class
@@ -39,19 +38,20 @@ if __name__ == "__main__":
     # Set up any important sub-fields in any of the tests.
     # Need to set an example program for simpl.
     example_sets['simpl'].partial_program = """
-fun arr1, arr2, l1, l2 ->
+fun arr, len, n ->
 r=0;
 while(?) {
 ?;
 r = r + 1;
 }
-return arr1;
+return arr;
 """
     example_sets['simpl'].int_comps = "0"
-    example_sets['simpl'].int_var_comps = 'l1, l2, r'
+    example_sets['simpl'].int_var_comps = 'n, len, r'
 
     base_case = gen_utils.L2Example()
-    base_case.two_array_input([], [])
+    base_case.add_array_input([])
+    base_case.add_int_input(2)
     base_case.array_output([])
     example_sets['L2'].base_cases = [base_case]
 
--- examples/array_inc/gen.py
+++ examples/array_inc/gen.py
@@ -8,22 +8,20 @@ def generate_example():
     array_len = random.randint(1, 30)
 
     arr1 = []
-    diff = random.randint(0, 100)
     res = []
 
     for i in range(array_len):
         n = random.randint(-100, 100)
         arr1.append(n)
-        res.append(n + diff)
+        res.append(n + 1)
 
-    return (arr1, diff, res)
+    return (arr1, res)
 
 
 # This tool is independent of the syntool name.
 def convert(example, example_class, syntool_name):
     example_class.add_array_input(example[0])
-    example_class.add_int_input(example[1])
-    example_class.array_output(example[2])
+    example_class.array_output(example[1])
 
     return example_class
 
@@ -38,7 +36,7 @@ if __name__ == "__main__":
     # Set up any important sub-fields in any of the tests.
     # Need to set an example program for simpl.
     example_sets['simpl'].partial_program = """
-fun arr, len, n ->
+fun arr, len ->
 r=0;
 while(?) {
 ?;
@@ -46,12 +44,11 @@ r = r + 1;
 }
 return arr;
 """
-    example_sets['simpl'].int_comps = "0"
+    example_sets['simpl'].int_comps = "0,1"
     example_sets['simpl'].int_var_comps = 'n, len, r'
 
     base_case = gen_utils.L2Example()
     base_case.add_array_input([])
-    base_case.add_int_input(2)
     base_case.array_output([])
     example_sets['L2'].base_cases = [base_case]
 
