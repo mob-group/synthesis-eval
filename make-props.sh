b57531e778881ff76be0c683590fdcbb7265a9ed
Bruce Collie <brucecollie82@gmail.com>
2019-10-23 16:17:21 +0100

    Initial script to make all the dummy props files

---
+++ make-props.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ROOT=$(realpath $(dirname "$0"))
+
+for example in $(ls "$ROOT/examples"); do
+  if [ ! -d "$ROOT/examples/$example" ]; then
+    continue
+  fi
+
+  IN_PROPS="$ROOT/examples/$example/props"
+  IN_C="$ROOT/examples/$example/ref.c"
+
+  if [ -f "$IN_PROPS" ]; then
+    exit 0
+  fi
+
+  if [ ! -f "$IN_C" ]; then
+    echo "No C implementation to copy: $IN_C"
+    exit 1
+  fi
+
+  cp "$IN_C" "$IN_PROPS"
+done
