dc0401107614226fd9e3b43b1e827dec6c5c2707
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2019-10-30 18:10:52 +0000

    Add a --test-file format that allows lists of inputs to be stored in a file

--- run-all.sh
+++ run-all.sh
@@ -8,6 +8,7 @@ help() {
 	echo "This script runs Makespeare, L2 and Simpl on a specified example."
 	echo "It puts the results in each directory"
 	echo "Specify tests with --test <name>, or run all with --all"
+	echo "Or, specify a file with test names using --test-file <filename>, using space separated test names"
 }
 
 if [[ $# -eq 0 ]]; then
@@ -15,8 +16,8 @@ if [[ $# -eq 0 ]]; then
 	exit 0
 fi
 
-typeset -a run_all run_tests
-zparseopts -D -E -- -all=run_all -test+:=run_tests
+typeset -a run_all run_tests test_files
+zparseopts -D -E -- -all=run_all -test+:=run_tests -test-file+:=test_files
 
 if [[ $# -ne 0 ]]; then
 	help
@@ -35,6 +36,20 @@ else
 	done
 fi
 
+if [[ ${#test_files} -gt 0 ]]; then
+	for arg in ${test_files[@]}; do
+		if [[ $arg != "--test-file" ]]; then
+			file_tests=( $(cat $arg) )
+			for file_test in ${file_tests[@]}; do
+				tests+=( $file_test )
+			done
+		fi
+	done
+fi
+
+echo "Running ${#tests} tests: "
+echo ${tests[@]}
+
 # We won't run any tests without a gen.py file.  Go through and make sure that each
 # gen.py file exists.
 typeset -a tests_to_run
@@ -47,7 +62,7 @@ for test in ${tests[@]}; do
 done
 
 if [[ ${#tests_to_run} -gt 0 ]]; then
-	parallel --line-buffer -j33% "echo 'starting test {}'; ./__run.sh {}; echo 'test {} done'" ::: ${tests_to_run[@]}
+	parallel --line-buffer -j100% "echo 'starting test {}'; ./__run.sh {}; echo 'test {} done'" ::: ${tests_to_run[@]}
 else
 	help
 	exit 0
