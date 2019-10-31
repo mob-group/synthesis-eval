12af067942c626cd0e4120c024e32760a0a61ee8
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2019-10-31 17:31:08 +0000

    Now check for timeouts also

--- __run.sh
+++ __run.sh
@@ -37,11 +37,22 @@ makespeare_out=examples/$test/makespeare_out
 l2_out=examples/$test/L2_out
 simpl_out=examples/$test/simpl_out
 
-timeout -s KILL $makespeare_timeout ./run-makespeare.sh $test > $makespeare_out &
-timeout -s KILL $l2_timeout ./run-L2.sh $test > $l2_out &
-timeout -s KILL $simpl_timeout ./run-simpl.sh $test > $simpl_out &
+tmp_makespeare_file=$(mktemp /tmp/syntheval.XXXXXX)
+tmp_l2_file=$(mktemp /tmp/syntheval.XXXXXX)
+tmp_simpl_timeout_file=$(mktemp /tmp/syntheval.XXXXXX)
+
+start_time=$( date '+%s')
+( timeout -s KILL $makespeare_timeout ./run-makespeare.sh $test &> $makespeare_out || true; echo $(date '+%s') > $tmp_makespeare_file) &
+(timeout -s KILL $l2_timeout ./run-L2.sh $test &> $l2_out || true; echo $( date '+%s') > $tmp_l2_file ) &
+(timeout -s KILL $simpl_timeout ./run-simpl.sh $test &> $simpl_out || true; echo $( date '+%s') > $tmp_simpl_timeout_file) &
 
 wait
+
+makespeare_end=$(cat $tmp_makespeare_file)
+l2_end=$(cat $tmp_l2_file)
+simpl_end=$(cat $tmp_simpl_timeout_file)
+
+# rm $tmp_makespeare_file $tmp_l2_file $tmp_simpl_timeout_file
 
 # Go through and attempt to determine whether the synthesis program succeeded.  Note
 # that for Makespeare, we cannot be sure that it succeeded when it claims success,
@@ -50,10 +61,13 @@ wait
 check_l2() {
 	local output_file=$1
 	local results_file=$2
+	local timed_out=$3
 
 	count=$(grep -e 'Found solution:' $output_file -c || true)
 	if [[ $count -ne 0 ]]; then
 		echo "L2: success" >> $results_file
+	elif [[ $timed_out == 1 ]]; then
+		echo "L2: timeout" >> $results_file
 	else
 		echo "L2: failed" >> $results_file
 	fi
@@ -62,11 +76,14 @@ check_l2() {
 check_makespeare() {
 	local output_file=$1
 	local results_file=$2
+	local timed_out=$3
 
 	count=$(grep -e 'Fully correct on training set' $output_file -c || true)
 
 	if [[ $count -ne 0 ]]; then
 		echo "makespeare: success" >> $results_file
+	elif [[ $timed_out == 1 ]]; then
+		echo "makespeare: timeout" >> $results_file
 	else
 		echo "makespeare: failed" >> $results_file
 	fi
@@ -75,21 +92,39 @@ check_makespeare() {
 check_simpl() {
 	local output_file=$1
 	local results_file=$2
+	local timed_out=$3
 
 	count=$(grep -e 'COMPLETE PROGRAM' $output_file -c || true)
 
 	if [[ $count -ne 0 ]]; then
 		echo "simpl: success" >> $results_file
+	elif [[ $timed_out == 1 ]]; then
+		echo "simpl: timeout" >> $results_file
 	else
 		echo "simpl: failed" >> $results_file
 	fi
 }
 
+timeout_check() {
+	# Print whether the window of time exceeds the timeout
+	# time.
+	local start_time=$1
+	local end_time=$2
+	local timeout_time=$3
+
+	if (( $end_time - $start_time > $timeout_time )); then
+		# timeout
+		echo 1
+	else
+		echo 0
+	fi
+}
+
 results_file=examples/$test/results
 echo "" -n > $results_file
-check_l2 $l2_out $results_file
-check_makespeare $makespeare_out $results_file
-check_simpl $simpl_out $results_file
+check_l2 $l2_out $results_file $(timeout_check $start_time $l2_end $l2_timeout)
+check_makespeare $makespeare_out $results_file $(timeout_check $start_time $makespeare_end $makespeare_timeout)
+check_simpl $simpl_out $results_file $(timeout_check $start_time $simpl_end $simpl_timeout)
 
 if [[ ${#check} -ne 0 ]]; then
 	echo "L2 Out:"
