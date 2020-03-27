750dcc65495e0acd2b54b8cb397e02cd55bb8e44
Bruce Collie <brucecollie82@gmail.com>
2020-03-27 09:32:00 +0000

    Small bug fix

--- examples/triangle_sum/ref.c
+++ examples/triangle_sum/ref.c
@@ -1,7 +1,7 @@
 int triangle_sum(int n) {
   int r = 0;
-  for (int i = 0; i < n; ++i) {
-    for (int m = 0; m < i; ++i) {
+  for (int i = 1; i < n; ++i) {
+    for (int m = 1; m < i; ++m) {
       r += m;
     }
   }
