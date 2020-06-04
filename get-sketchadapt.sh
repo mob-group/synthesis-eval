348ad7c23a38689822c6532bef81d4d90e121c5c
Jackson Woodruff <s1988171@spa.inf.ed.ac.uk>
2020-06-04 12:25:28 +0100

    Fix the get/run sketchadapt scripts so they actually work

--- get-sketchadapt.sh
+++ get-sketchadapt.sh
@@ -2,7 +2,7 @@
 set -eu
 
 ROOT=$(realpath $(dirname "$0"))
-INSTALL_DIR="$ROOT/SketchAdapt"
+INSTALL_DIR="$ROOT/neural_sketch"
 
 if [ -d "$INSTALL_DIR"]; then
 	echo "Removing existing SketchAdapt dir..."
@@ -13,6 +13,11 @@ echo "Installing SketchAdapt..."
 git clone https://github.com/j-c-w/neural_sketch
 
 cd neural_sketch
+# Get onto the right branch
+git fetch
+git checkout origin/working-mnye
+# And get out of detachted head state
+git checkout -b working-mnye
 git submodule update --init --recursive
 # Get the dependencies
 python3 -m virtualenv env
@@ -20,5 +25,7 @@ source env/bin/activate
 
 pip3 install torch==1.4.0 numpy dill matplotlib
 cd program_synthesis
-pip3 install -e
+pip3 install -e .
+
+./install_eval.sh
 cd ..
--- run-sketchadapt.sh
+++ run-sketchadapt.sh
@@ -1,1 +1,22 @@
-# TODO
+#!/usr/bin/env bash
+
+set -eu
+
+ROOT=$(realpath $(dirname "$0"))
+INSTALL_DIR="$ROOT/neural_sketch"
+
+IN_FILE="$ROOT/examples/$1/SketchAdapt"
+
+if [[ ! -f "$IN_FILE" ]]; then
+	echo "SketchAdapt input $IN_FILE does not exist"
+	exit 1
+fi
+
+# Now run the algorithm
+cd $INSTALL_DIR
+source env/bin/activate
+
+# And get the pypy installation
+export PATH=$PATH:pypy3.6-v7.3.1-linux64/bin
+
+python3 eval/evaluate_deepcoder.py --precomputed_data_file $IN_FILE
