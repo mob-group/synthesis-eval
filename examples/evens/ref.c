42745f3769ce22a1fad91c8d97897419c46f1a0f
Jackson Woodruff <s1988171@spa.inf.ed.ac.uk>
2020-06-04 12:26:11 +0100

    Add evens example from the sketch adapt paper

---
+++ examples/evens/ref.c
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+
+void evens(int *a, int *b, int n) {
+  int outindex = 0;
+  for (int i = 0; i < n; ++i) {
+    if (a[i] % 2 == 0) {
+		b[outindex] = a[i];
+		outindex ++;
+    }
+  }
+}
