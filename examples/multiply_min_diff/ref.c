a762fa47184c902cdfe756a3273c60081f01bca5
Jackson Woodruff <s1988171@spa.inf.ed.ac.uk>
2020-07-17 21:36:48 +0100

    Fix broken c code

--- examples/multiply_min_diff/ref.c
+++ examples/multiply_min_diff/ref.c
@@ -1,5 +1,5 @@
 void multiply_min_diff(int *arr, int *arr2, int n, int *out) {
-  int diff_so_far = 10000
+  int diff_so_far = 10000;
   for (int i = 0; i < n; i ++) {
 	  int diff = arr[i] - arr2[i];
 	  if (diff < diff_so_far) {
