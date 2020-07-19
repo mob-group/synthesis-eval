0fc1c37296367b90e14b36a513ecf39c6d095aa8
Bruce Collie <brucecollie82@gmail.com>
2020-07-19 11:20:02 +0100

    Another one

--- examples/evens/ref.c
+++ examples/evens/ref.c
@@ -4,8 +4,8 @@ void evens(int *a, int *b, int n) {
   int outindex = 0;
   for (int i = 0; i < n; ++i) {
     if (a[i] % 2 == 0) {
-		b[outindex] = a[i];
-		outindex ++;
+      b[outindex] = a[i];
+      outindex++;
     }
   }
 }
