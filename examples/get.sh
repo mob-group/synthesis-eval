3cbe2fb9e0bf7168c0b548e9820790455596944e
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2022-10-17 23:18:07 +0100

    Fix script

--- examples/get.sh
+++ examples/get.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo -n "" > full.json
+
 for f in $(find -name gen.py); do
 	dname=$(dirname $f)
 	echo "LOokng at $dname"
 	cd $dname
 	python gen.py
 	mv LLM LLM.json
+	cat LLM.json >> ../full.json
+	echo "" >> ../full.json
 	cd ..
 done
